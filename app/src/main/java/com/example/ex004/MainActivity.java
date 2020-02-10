9adcf8bdbb3cca75b5a24059ef5fbe41a3e121a7
liadp <lp8186@bs.amalnet.k12.il>
2020-02-10 19:19:48 +0200

    Working!

---
+++ app/src/main/java/com/example/ex004/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ex004;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn= (Button) findViewById(R.id.btn);
+        count=0;
+    }
+
+    public void push(View view) {
+        count= count+1;
+        if ((count%7)!=0){
+            btn.setText("This is a click number"+count);
+        }
+        else{
+            btn.setText("BOOM");
+        }
+    }
+}
